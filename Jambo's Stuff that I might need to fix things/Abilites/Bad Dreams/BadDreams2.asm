2c45282bd63d8f953083b9ff15d715fe25a9f2fa
aiudirog <aiudirog@github.com>
2013-12-10 16:36:56 -0500

    Found stuff for bad dreams

---
+++ Jambo's Stuff that I might need to fix things/Abilites/Bad Dreams/BadDreams2.asm
@@ -0,0 +1,21 @@
+.text
+.align 2
+.thumb
+.thumb_func
+.global baddreams
+
+main:
+	ldr r0, place
+	ldrb r0, [r0, #0x0]
+	mov r1, #0x58
+	mul r0, r1
+	ldr r1, place2
+	add r0, r0, r1
+	ldr r1, storageloc
+	ldr r2, [r1, #0x0]
+	str r2, [r0, #0x0]
+	bx lr
+.align
+place:	.word 0x02023D6B
+place2:	.word 0x02023C34
+storageloc:	.word 0x0203D000
